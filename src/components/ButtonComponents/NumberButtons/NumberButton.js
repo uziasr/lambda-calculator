6df4aa89e57f1ee03e36c315f91885dd57adaa04
Uzias Rivera <uziasrivera@gmail.com>
2019-09-10 15:06:16 -0700

    connected the three components

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const NumberButton = () => {
+export const NumberButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className=''>{props.number}
+      </button>
+
     </>
   );
 };
+
+
